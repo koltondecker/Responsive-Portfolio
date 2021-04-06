9cdd9b5e44314275d144dbcaa7f6e680f7042daf
Kolton Decker <deckerman25@gmail.com>
2021-04-05 21:49:47 -0500

    Updated home page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const elems = document.querySelectorAll('.parallax');
-    const instances = M.Parallax.init(elems);
+
+    const parallaxElems = document.querySelectorAll('.parallax');
+    const collapsibleElems = document.querySelectorAll('.collapsible');
+
+    M.Collapsible.init(collapsibleElems);
+    M.Parallax.init(parallaxElems);
+
+
 });
