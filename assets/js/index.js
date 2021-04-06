72c415733a7daa2e5eae62a6810172471c491c2e
Kolton Decker <deckerman25@gmail.com>
2021-04-06 11:08:11 -0500

    Added source code and deployed project links to each project section on portfolio page.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const parallaxElems = document.querySelectorAll('.parallax');
     const collapsibleElems = document.querySelectorAll('.collapsible');
+    var sideNavElems = document.querySelectorAll('.sidenav');
+    
 
     M.Collapsible.init(collapsibleElems);
     M.Parallax.init(parallaxElems);
-
+    M.Sidenav.init(sideNavElems);
 
 });
