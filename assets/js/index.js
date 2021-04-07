5fe8febc70358c37095596db88321894c1549074
Kolton Decker <deckerman25@gmail.com>
2021-04-06 21:06:12 -0500

    Cleaning up commented items

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,4 @@ $(document).ready(function(){
 
     $('.sidenav').sidenav();
 
-    // $(".project-link").hover((e) => {
-    //     const icon = e.target;
-    //     console.log(icon);
-
-    //     e.target.addAttribute("transform", "scale(1.1)");
-    // })
-
 });
